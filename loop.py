002818b4bb2665bd0740851f09aa9fd2f4146ca1
Alfred Lua <alfredlua@users.noreply.github.com>
2025-01-16 17:25:39 +0800

    Support Claude without tools

--- loop.py
+++ loop.py
@@ -7,13 +7,19 @@ from dotenv import load_dotenv
 from onboarding_message import intro
 
 import google.generativeai as genai
+import anthropic
 
 from tools.screenshot import save_screenshot
-from tools.browser import scrape_source
+from tools.browser import scrape_static_source, scrape_dynamic_source
 from tools.parser import parse_text
 
+import inquirer
+
 load_dotenv()
-api_key=os.getenv("GOOGLE_GEMINI_API_KEY")
+gemini_api_key=os.getenv("GOOGLE_GEMINI_API_KEY")
+anthropic_api_key=os.getenv("ANTHROPIC_API_KEY")
+
+# System prompt
 
 # This system prompt is taken from Claude computer use. I only added my computer's architecture (arm64).
 # From Anthropic: This system prompt is optimized for the Docker environment in this repository and
@@ -55,46 +61,131 @@ Model: Thinking... I need to scrape https://news.ycombinator.com and find the fi
 </IMPORTANT>
 """
 
+# Main loop
+
 async def loop():
   """
   The loop for the assistant.
   """
-  genai.configure(api_key=api_key)
+  tool_collection = [save_screenshot, scrape_static_source, scrape_dynamic_source, parse_text]
 
-  tool_collection = [save_screenshot, scrape_source, parse_text]
+  # Anthropic declares tools differently
+
   functions = {
-    "scrape_source": scrape_source,
+    "scrape_static_source": scrape_static_source,
+    "scrape_dynamic_source": scrape_dynamic_source,
     "parse_text": parse_text,
     "save_screenshot": save_screenshot
   }
 
-  def call_function(function_call):
-    function_name = function_call.name
-    function_args = function_call.args
-    return functions[function_name](**function_args)
-  
-  def process_response(response):
-    queue = [response]  # Use a queue to track responses
-    while queue:
-      current_response = queue.pop(0)
-      for part in current_response.candidates[0].content.parts:
-        if part.function_call:
-          tool_result = call_function(part.function_call)
-          print("🛠️  Tool result:", tool_result, "\n\n------------\n")
-          tool_response = chat.send_message(tool_result)
-          queue.append(tool_response)  # Add the new response to the queue
-        else:
-          if part.text.strip():
-            print("\n🤵🏻 Aura: ", part.text)
-
-  model = genai.GenerativeModel(
+  genai.configure(api_key=gemini_api_key)
+  gemini_model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     system_instruction=SYSTEM_PROMPT,
     tools=tool_collection)
-  
-  chat = model.start_chat()
 
-  print(f"{intro}🤵🏻 Aura: Hello! How can I help you today? (Type 'quit' to end the chat)\n")
+  claude_model = anthropic.Anthropic()
+
+  messages = []
+
+  # Helper functions
+
+  def generate_text(model, messages):
+    if model == "gemini":
+      return gemini_model.generate_content(messages)
+    elif model == "claude":
+      return claude_model.messages.create(
+        model="claude-3-haiku-20240307", 
+        messages=messages,
+        max_tokens=2048
+      )
+
+  def call_function(function_call):
+      function_name = function_call.name
+      function_args = function_call.args
+      return functions[function_name](**function_args)
+    
+  def process_response(response):
+    queue = [response]
+    while queue:
+      current_response = queue.pop(0)
+      if selected_model == "gemini":
+        messages.append({"role": "model", message_key: current_response.candidates[0].content.parts})
+        for part in current_response.candidates[0].content.parts:
+          if part.function_call:
+            tool_result = call_function(part.function_call)
+            print("🛠️  Tool result:", tool_result, "\n\n------------\n")
+            messages.append({"role": "user", message_key: [{"text": f"Here is the result from the tool: {tool_result}"}]})
+            tool_response = generate_text(selected_model, messages)
+            queue.append(tool_response) 
+          else:
+            if part.text.strip():
+              print(f"\n🤵🏻 Aura ({selected_model}): {part.text}")
+      elif selected_model == "claude":
+        if "error" in current_response:
+           return f"An error occurred: {current_response['error']}"
+
+        # if response.content[-1].type == "tool_use":
+        #     tool_use = response.content[-1]
+        #     func_name = tool_use.name
+        #     func_params = tool_use.input
+        #     tool_use_id = tool_use.id
+
+        #     result = handle_tool_use(func_name, func_params)
+        #     messages.append(
+        #         {"role": "assistant", "content": response.content}
+        #     )
+        #     messages.append({
+        #         "role": "user",
+        #         "content": [{
+        #             "type": "tool_result",
+        #             "tool_use_id": tool_use_id,
+        #             "content": f"{result}",
+        #         }],
+        #     })
+
+        #     follow_up_response = self.generate_message(
+        #         messages=messages,
+        #         max_tokens=2048,
+        #     )
+
+        #     if "error" in follow_up_response:
+        #         return f"An error occurred: {follow_up_response['error']}"
+
+        #     response_text = follow_up_response.content[0].text
+        #     messages.append(
+        #         {"role": "assistant", "content": response_text}
+        #     )
+        #     return response_text
+        
+        if current_response.content[0].type == "text":
+            response_text = current_response.content[0].text
+            messages.append(
+                {"role": "assistant", "content": response_text}
+            )
+            print(f"\n🤵🏻 Aura ({selected_model}): {response_text}")
+        
+        else:
+            raise Exception("An error occurred: Unexpected response type")
+
+  print(f"{intro}🤵🏻 Aura: Hello! To get started, select an AI model that will power me. (Type 'quit' to end the chat)\n")
+
+  question = [
+    inquirer.List(
+      'choice',
+      message="Choose an AI model",
+      choices=['gemini', 'claude'],
+    ),
+  ]
+
+  message_key = ""
+  selected_model = inquirer.prompt(question)['choice']
+  if selected_model == 'gemini':
+    message_key = "parts"
+    print("You chose gemini.")
+  elif selected_model == 'claude':
+    message_key = "content"
+    print("You chose claude.")
 
   while True:
     try:
@@ -102,23 +193,27 @@ async def loop():
       if user_message.lower() == "quit":
         print("Exiting chat.")
         break
+      elif user_message.lower() == "restart":
+        messages = []
+        continue
       elif user_message.lower() == "debug":
-        print("Chat History\n", chat.history)
-        break
+        print("Chat History\n", messages)
+        continue
       
-      response = chat.send_message(user_message)
-      response.resolve()
-
+      messages.append({"role": "user", message_key: user_message})
+      response = generate_text(selected_model, messages)
       process_response(response)
 
-      summary = chat.send_message(f"Summarize what the model did in first-person narrative. Do not use any tools: {chat.history}")
-      print("\n🤵🏻 Aura: Here's a summary of what I did:\n\n", summary.text)
+      # summary = generate_text(selected_model, f"Summarize what the model did in first-person narrative. Do not use any tools: {messages}")
+      # print(f"\n🤵🏻 Aura ({selected_model}): Here's a summary of what I did:\n\n {summary.text}")
 
     except Exception as e:
       import traceback
       print(f"An error occurred: {e}")
       print("Error type:", type(e).__name__)
       print("Traceback:", traceback.format_exc())
+
+# Run script
 
 if __name__ == "__main__":
   import asyncio
--- tools/browser.py
+++ tools/browser.py
@@ -22,7 +22,7 @@ def visit_website(url: str):
 
   return f"I have opened {url}."
 
-def scrape_source(url: str):
+def scrape_static_source(url: str):
   """
   Scrape the source of a given website.
   
@@ -35,7 +35,7 @@ def scrape_source(url: str):
   import requests
   from bs4 import BeautifulSoup
 
-  print(f'🤵🏻 Scraping the web source of {url}...\n')
+  print(f'🤵🏻 Scraping the web source of {url} with Beautiful Soup...\n')
 
   response = requests.get(url)
   if response.status_code == 200:
