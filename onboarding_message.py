083086a2c01c1726ecba513b2eabca53a8166ded
Alfred Lua <alfredlua@users.noreply.github.com>
2025-01-20 21:42:26 +0800

    Fix onboarding message

--- onboarding_message.py
+++ onboarding_message.py
@@ -1,10 +1,10 @@
 intro = """
 +---------------------------------------------------------------------------------------+
-|   AAAAA   U   U  RRRRRR    AAAAA                                                        |
-|  A     A  U   U  R    R   A     A                                                      |
-|  AAAAAAA  U   U  RRRRRR   AAAAAAA                                                      |
-|  A     A  U   U  R   R    A     A                                                      |
-|  A     A  UUUUU  R    R   A     A                                                      |
+|   AAAAA   U   U  RRRRRR    AAAAA                                                      |
+|  A     A  U   U  R    R   A     A                                                     |
+|  AAAAAAA  U   U  RRRRRR   AAAAAAA                                                     |
+|  A     A  U   U  R   R    A     A                                                     |
+|  A     A  UUUUU  R    R   A     A                                                     |
 |                                                                                       |
 | Hello there, I'm Aura, your personal assistant. For now, I can help you visit and     |
 | summarize websites. To get started, you can try something like "Go to hacker news,    |
